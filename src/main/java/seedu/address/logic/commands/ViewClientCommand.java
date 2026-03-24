04a34605af440c8f44a448aef69a7f54758284e0
wuikong <126385630+wuikong@users.noreply.github.com>
2026-03-24 20:11:42 +0800

    Deduplicate displayed property list

--- src/main/java/seedu/address/logic/commands/ViewClientCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewClientCommand.java
@@ -37,8 +37,7 @@ public class ViewClientCommand extends Command {
 
         model.updateFilteredPersonList(p -> p.isSamePerson(personToView));
         model.updateFilteredPropertyList(
-                p -> lastShownList.stream().anyMatch(person -> person.getProperties().contains(p)
-                        && person.isSamePerson(personToView)));
+                p -> lastShownList.stream().anyMatch(person -> person.getProperties().contains(p)));
 
         return new CommandResult(
                 String.format(Messages.MESSAGE_PROPERTIES_LISTED_OVERVIEW, model.getFilteredPropertyList().size()));
